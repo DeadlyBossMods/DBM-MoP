41b18a2d94ff17fe9fabc0f24786344b2402830f
nbluewiz <nbluewiz@1bdcc323-82bc-47a7-ad08-65ab6bbe8a6f>
2015-01-19 08:05:52 +0000

    # kill bad flag.

--- trunk/DBM-ThroneofThunder/CouncilofElders.lua
+++ trunk/DBM-ThroneofThunder/CouncilofElders.lua
@@ -62,8 +62,8 @@ local specWarnSandBolt				= mod:NewSpecialWarningInterrupt(136189, false)
 local specWarnSandStorm				= mod:NewSpecialWarningSpell(136894, nil, nil, nil, 2)
 local specWarnQuickSand				= mod:NewSpecialWarningMove(136860)
 --High Prestess Mar'li
-local specWarnBlessedLoaSpirit		= mod:NewSpecialWarningSwitch(137203, "RangeDps")--Ranged should handle this, melee chasing it around is huge dps loss for possessed. On 10 man 2 ranged was enough. If you do not have 2 ranged, 1 or 2 melee will have to help and probably turn this on manually
-local specWarnShadowedLoaSpirit		= mod:NewSpecialWarningSwitch(137350, "RangeDps")
+local specWarnBlessedLoaSpirit		= mod:NewSpecialWarningSwitch(137203, "RangedDps")--Ranged should handle this, melee chasing it around is huge dps loss for possessed. On 10 man 2 ranged was enough. If you do not have 2 ranged, 1 or 2 melee will have to help and probably turn this on manually
+local specWarnShadowedLoaSpirit		= mod:NewSpecialWarningSwitch(137350, "RangedDps")
 local specWarnMarkedSoul			= mod:NewSpecialWarningRun(137359, nil, nil, nil, 4)
 local specWarnTwistedFate			= mod:NewSpecialWarningSwitch(137891)
 --Frost King Malak
