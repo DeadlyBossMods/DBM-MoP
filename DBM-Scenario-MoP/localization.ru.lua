f6ad86aa70b3db71d7b6b4efa8e7c8c29f36023d
Kris <cblair91@gmail.com>
2019-08-14 22:26:01 +0100

    Fix RU locales

--- DBM-Scenario-MoP/localization.ru.lua
+++ DBM-Scenario-MoP/localization.ru.lua
@@ -10,17 +10,17 @@ L = DBM:GetModLocalization("d517")
 
 L:SetTimerLocalization({
 	timerEvent	= "Варка хмеля (приблиз.)"
-}
+})
 
 L:SetOptionLocalization({
 	timerEvent	= "Отсчет времени до окончания варки хмеля"
-}
+})
 
 L:SetMiscLocalization({
 	BrewStart		= "Гроза начинается! Готовьтесь.",
 	BorokhulaPull	= "Последний шанс, червяки вы гнилозубые!",
 	BorokhulaAdds	= "просит подкрепления."--In case useful/important on heroic. On normal just zerg boss and ignore these unless you want achievement.
-}
+})
 
 -----------------------
 -- A Little Patience --
@@ -29,7 +29,7 @@ L = DBM:GetModLocalization("d589")
 
 L:SetMiscLocalization({
 	ScargashPull	= "Вы – слабы!"--Not yet in use but could be with more logs and combat start timers
-}
+})
 
 ---------------------------
 -- Arena Of Annihilation --
@@ -43,7 +43,7 @@ L = DBM:GetModLocalization("d593")
 
 L:SetMiscLocalization({
 	TelvrakPull	= "Зан'весс никогда не падет!"
-}
+})
 
 ------------------------------
 -- Battle on the High Seas ---
@@ -62,18 +62,18 @@ L = DBM:GetModLocalization("d539")
 
 L:SetTimerLocalization({
 	timerBossCD	= "Прибытие: %s"
-}
+})
 
 L:SetOptionLocalization({
 	timerBossCD	= "Отсчет времени до появления следующего босса"
-}
+})
 
 L:SetMiscLocalization({
 	RatEngage	= "Это же мать логова!",
 	BeginAttack	= "Мы должны защитить жителей деревни!",
 	Yeti		= "Батаарский боевой йети",
 	Qobi		= "Воитель Коби"
-}
+})
 
 ------------------------------
 -- Crypt of Forgotten Kings --
@@ -87,15 +87,15 @@ L = DBM:GetModLocalization("d616")
 
 L:SetTimerLocalization({
 	timerAddsCD	= "Восст. Призыв Аддов"
-}
+})
 
 L:SetOptionLocalization({
 	timerAddsCD	= "Отсчет времени до восстановления призыва аддов у Чешуйчатого повелителя"
-}
+})
 
 L:SetMiscLocalization({
 	LizardLord	= "Пещеру сторожат сауроки. Займемся ими."
-}
+})
 
 ----------------------------
 -- Dark Heart of Pandaria --
@@ -104,7 +104,7 @@ L = DBM:GetModLocalization("d647")
 
 L:SetMiscLocalization({
 	summonElemental	= "Прислужники, уничтожьте этих букашек!"
-}
+})
 
 ------------------------
 -- Greenstone Village --
@@ -118,11 +118,11 @@ L = DBM:GetModLocalization("Landfall")
 
 L:SetWarningLocalization({
 	WarnAchFiveAlive	= "Достижение \"Неубиваемая пятерка\" провалено"
-}
+})
 
 L:SetOptionLocalization({
 	WarnAchFiveAlive	= "Показывать предупреждение, если достижение \"Неубиваемая пятерка\" провалено"
-}
+})
 
 ----------------------------
 -- The Secret of Ragefire --
@@ -132,7 +132,7 @@ L = DBM:GetModLocalization("d649")
 L:SetMiscLocalization({
 	XorenthPull		= "Низшие расы – враги истинной Орды!",
 	ElagloPull		= "Глупцы! Истинную Орду такие, как вы, не остановят."
-}
+})
 
 ----------------------
 -- Theramore's Fall --
@@ -157,21 +157,21 @@ L = DBM:GetModLocalization("d594")
 L:SetWarningLocalization({
 	specWarnLostSouls		= "Заблудшие души!",
 	specWarnEnslavePitLord	= "Властитель преисподней - Поработите демона!"
-}
+})
 
 L:SetTimerLocalization({
 	timerLostSoulsCD	= "Восст. Заблудшие души"
-}
+})
 
 L:SetOptionLocalization({
 	specWarnLostSouls		= "Спец-предупреждение, когда появляются Заблудшие души",
 	specWarnEnslavePitLord	= "Спец-предупреждение поработить демона, когда Властитель преисподней активируется/освобождается",
 	timerLostSoulsCD		= "Отсчет времени до появления следующих Заблудших душ"
-}
+})
 
 L:SetMiscLocalization({
 	LostSouls	= "Face the souls of those your kind doomed to perish, Warlock!"
-}
+})
 
 -------------------------------
 -- Finding Secret Ingredient --
